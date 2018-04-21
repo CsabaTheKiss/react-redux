4541eb2c72376e7c310ab90afc4be7321cebc587
Csaba Kiss2 <Csaba_Kiss2@epam.com>
2018-04-21 16:52:57 +0200

    updated reducer

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
+import * as actionTypes from '../../store/actions';
+
 class Counter extends Component {
     render () {
         return (
@@ -13,6 +15,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={this.props.onAddToCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractFromCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store result</button>
+                <ul>
+                    {this.props.storedResults.map(result => {
+                        return <li key={result.id} onClick={() => this.props.onDeleteResult(result.id)}>{result.value}</li>
+                    })}
+                </ul>
             </div>
         );
     }
@@ -21,17 +30,20 @@ class Counter extends Component {
 // in which parts of the store is this component interested
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 }
 
 // in which dispatch actions is this component interested
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
-        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
-        onAddToCounter: () => dispatch({type: 'ADD', value: 5}),
-        onSubtractFromCounter: () => dispatch({type: 'SUBTRACT', value: 5}),
+        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
+        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
+        onAddToCounter: () => dispatch({type: actionTypes.ADD, value: 5}),
+        onSubtractFromCounter: () => dispatch({type: actionTypes.SUBTRACT, value: 5}),
+        onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, id })
     };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,21 +1,46 @@
+import * as actionTypes from './actions';
+
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'INCREMENT':
-            return { counter: state.counter + 1 };
-        case 'DECREMENT':
-            return { counter: state.counter - 1 };
-        case 'ADD':
-            return { counter: state.counter + action.value };
-        case 'SUBTRACT':
-            return { counter: state.counter - action.value };
+        case actionTypes.INCREMENT:
+            return { 
+                ...state, 
+                counter: state.counter + 1 
+            };
+        case actionTypes.DECREMENT:
+            return {
+                ...state, 
+                counter: state.counter - 1 
+            };
+        case actionTypes.ADD:
+            return {
+                ...state, 
+                counter: state.counter + action.value
+            };
+        case actionTypes.SUBTRACT:
+            return {
+                ...state, 
+                counter: state.counter - action.value
+            };
+        case actionTypes.STORE_RESULT:
+            return { 
+                ...state,
+                results: state.results.concat({id: new Date() + Math.random(), value: state.counter}) 
+            };
+        case actionTypes.DELETE_RESULT:
+        const updatedArray = state.results.filter(result => result.id !== action.id)
+            return {
+                ...state,
+                results: updatedArray
+            };
         default:
             return state;
     }
-    return state;
 }
 
 export default reducer;
